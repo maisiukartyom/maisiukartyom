72af0147b6030c9b4e93d1e20d27a215a69e40b2
Artyom Maisiuk <maisiukartyom@gmail.com>
2021-11-13 21:12:26 +0300

    Add files via upload

---
+++ JS_Block.js
@@ -0,0 +1,31 @@
+
+// Задание 1
+function task1() {
+	const stringCases = myString => myString[0].toUpperCase() + myString.toLowerCase().slice(1)
+
+	document.getElementById('result').defaultValue = ''
+	document.getElementById('result').defaultValue = stringCases(prompt('Введите слово')) 
+}
+//
+
+// Задание 2
+function task2() {
+	name = prompt('Как вас зовут?')
+	const greet = name => name === 'Mark' ? alert('Hi, Mark!') : alert(`Hello, ${name}!`)
+
+	greet(name)
+}
+//
+
+// Задание 3
+function task3(){
+	function filterStrings(arr, count){
+		return arr.filter(word => {if (word.length <= count) return word})
+	}
+
+	document.getElementById('result').defaultValue = ''
+	document.getElementById('result').defaultValue = filterStrings(prompt('Перечислите строки через запятую').split(", "), parseInt(prompt('Введите MAX длинну слова'), 10))
+	console.log(filterStrings(['I love JS', 'some very long string', 'hell'], 9))
+	console.log(filterStrings(['a', 'ab', 'abc'], 2))
+}
+//
